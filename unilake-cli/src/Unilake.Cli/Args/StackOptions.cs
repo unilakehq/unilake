c8613e0cc0f5d6e77074cb12f9e329e003fe2aad
Menno Hamburg <menno@mhamburg.nl>
2023-12-01 12:40:19 +0100

    chore: added CLI dependency checks

---
+++ unilake-cli/src/Unilake.Cli/Args/StackOptions.cs
@@ -0,0 +1,46 @@
+﻿using CommandLine;
+using OneOf;
+using Unilake.Cli.Args;
+using Unilake.Cli.Config;
+using Parser = Unilake.Cli.Config.Parser;
+
+namespace Unilake.Cli;
+
+public abstract class StackOptions : Options
+{
+    [Option('c', "config-file", Required = false, HelpText = "Config file location.")]
+    public string? ConfigFile { get; set; }
+    private bool FileBased => !string.IsNullOrWhiteSpace(ConfigFile);
+
+    protected async Task<OneOf<EnvironmentConfig, int>> EnvironmentChecks()
+    {
+        // check config file (presence)
+        if (FileBased && !File.Exists(ConfigFile))
+        {
+            Console.Error.WriteLine($"Config file not found: {ConfigFile}");
+            return 1;
+        }
+
+        // check config file (correctness)
+        var parsed = FileBased ? Parser.ParseFromPath(ConfigFile!) : Parser.ParseFromEmbeddedResource("Unilake.Cli.unilake.default.yaml");
+        if (!parsed.IsValid())
+        {
+            parsed.PrettyPrintErrors();
+            return 1;
+        }
+
+        // check dependencies (kubectl, pulumi)
+        var deps = new Dictionary<string, string[]>{
+            {"pulumi", new[] {"version"}},
+            {"kubectl", new[] {"version"}}
+        };
+        var dep_result = await StackContext.CheckEnvironmentDependenciesAsync(deps);
+
+        StackContext.ProcessCheckEnvironmentDependenciesResult(dep_result);
+
+        if (!dep_result.isSuccess)
+            return 1;
+
+        return parsed;
+    }
+}
--- unilake-cli/src/Unilake.Cli/Args/UpOptions.cs
+++ unilake-cli/src/Unilake.Cli/Args/UpOptions.cs
@@ -1,37 +1,23 @@
 using CommandLine;
 using Unilake.Cli.Stacks;
-using Parser = Unilake.Cli.Config.Parser;
 
 namespace Unilake.Cli.Args;
 
 [Verb("up", HelpText = "Create or update a UniLake deployment.")]
-public class UpOptions : Options
+public class UpOptions : StackOptions
 {
-    [Option('c', "config-file", Required = false, HelpText = "Config file location.")]
-    public string? ConfigFile { get; set; }
-
     [Option('f', "skip-preview", Required = false, HelpText = "Skip previewing changes before applying.")]
     public bool Force { get; set; } = false;
-
-    private bool FileBased => !string.IsNullOrWhiteSpace(ConfigFile);
 
     public override async Task<int> ExecuteAsync(CancellationToken cancellationToken)
     {
         Console.WriteLine("Running up command...");
-        if (FileBased && !File.Exists(ConfigFile))
-        {
-            Console.Error.WriteLine($"Config file not found: {ConfigFile}");
-            return 1;
-        }
 
-        var parsed = FileBased ? Parser.ParseFromPath(ConfigFile!) : Parser.ParseFromEmbeddedResource("Unilake.Cli.unilake.default.yaml");
-        if (!parsed.IsValid())
-        {
-            parsed.PrettyPrintErrors();
-            return 1;
-        }
-        
-        var workspace = await new StackHandler<Kubernetes>(new Kubernetes(parsed)).InitWorkspace("someProject", "someStack", cancellationToken);
+        // run stack
+        var parsed = await EnvironmentChecks();
+        if (parsed.IsT1)
+            return parsed.AsT1;
+        var workspace = await new StackHandler<Kubernetes>(new Kubernetes(parsed.AsT0)).InitWorkspace("someProject", "someStack", cancellationToken);
         await workspace.InstallPluginsAsync(cancellationToken);
         var result = await workspace.UpAsync(cancellationToken);
         if (result == null)
