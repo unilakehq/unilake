34510fd30adf8cc9af3b11d1c648f693f7843b89
Menno Hamburg <menno.hamburg@unilake.com>
2023-12-27 11:48:58 +0000

    Initial commit of changes

---
+++ unilake-web/www/src/Unilake.Www/Pages/Shared/BlogSlider.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Unilake.Www.Pages.Shared
+{
+    public class BlogSliderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
