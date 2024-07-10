3d8028c50fafa5621d38e80905425dc97fe880e4
Menno Hamburg <menno@mhamburg.nl>
2024-07-10 10:21:43 +0000

    Sonarcloud issue

--- unilake-integration-sdk/Airbyte.Cdk/Sources/Utils/CasingExtension.cs
+++ unilake-integration-sdk/Airbyte.Cdk/Sources/Utils/CasingExtension.cs
@@ -9,21 +9,21 @@ namespace Airbyte.Cdk.Sources.Utils
         /// <summary>
         /// Convert string to snake case formatting
         /// </summary>
-        public static string ToSnakeCase(this string str) => 
+        public static string ToSnakeCase(this string str) =>
             string.Concat((str ?? string.Empty).Select((x, i) => i > 0 && char.IsUpper(x) && !char.IsUpper(str[i - 1]) ? $"_{x}" : x.ToString())).ToLower();
-        
+
         /// <summary>
         /// Convert string to pascal case formatting
         /// </summary>
         public static string ToPascalCase(this string str)
         {
             //SOURCE: https://stackoverflow.com/questions/18627112/how-can-i-convert-text-to-pascal-case
-            Regex invalidCharsRgx = new Regex("[^_a-zA-Z0-9]");
-            Regex whiteSpace = new Regex(@"(?<=\s)");
-            Regex startsWithLowerCaseChar = new Regex("^[a-z]");
-            Regex firstCharFollowedByUpperCasesOnly = new Regex("(?<=[A-Z])[A-Z0-9]+$");
-            Regex lowerCaseNextToNumber = new Regex("(?<=[0-9])[a-z]");
-            Regex upperCaseInside = new Regex("(?<=[A-Z])[A-Z]+?((?=[A-Z][a-z])|(?=[0-9]))");
+            Regex invalidCharsRgx = new Regex("[^_a-zA-Z0-9]", RegexOptions.None, TimeSpan.FromSeconds(2));
+            Regex whiteSpace = new Regex(@"(?<=\s)", RegexOptions.None, TimeSpan.FromSeconds(2));
+            Regex startsWithLowerCaseChar = new Regex("^[a-z]", RegexOptions.None, TimeSpan.FromSeconds(2));
+            Regex firstCharFollowedByUpperCasesOnly = new Regex("(?<=[A-Z])[A-Z0-9]+$", RegexOptions.None, TimeSpan.FromSeconds(2));
+            Regex lowerCaseNextToNumber = new Regex("(?<=[0-9])[a-z]", RegexOptions.None, TimeSpan.FromSeconds(2));
+            Regex upperCaseInside = new Regex("(?<=[A-Z])[A-Z]+?((?=[A-Z][a-z])|(?=[0-9]))", RegexOptions.None, TimeSpan.FromSeconds(2));
 
             // replace white spaces with underscore, then replace all invalid chars with empty string
             var pascalCase = invalidCharsRgx.Replace(whiteSpace.Replace(str, "_"), string.Empty)
