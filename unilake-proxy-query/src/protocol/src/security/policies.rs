48a832928f6551309652b2c864aabd89be14146c
Menno Hamburg <menno@mhamburg.nl>
2024-11-05 11:27:13 +0100

    policymanager polishing and tests added

--- unilake-proxy-query/src/protocol/src/security/policies.rs
+++ unilake-proxy-query/src/protocol/src/security/policies.rs
@@ -1,3 +1,5 @@
+use base64::prelude::BASE64_STANDARD;
+use base64::Engine;
 use casbin::{Cache, EventData, Logger};
 use std::hash::{DefaultHasher, Hash, Hasher};
 use std::sync::mpsc::{channel, Receiver, Sender};
@@ -22,19 +24,38 @@ pub(crate) struct HitRule {
     rules: Vec<String>,
 }
 
-pub(crate) struct PolicyHitLogger {
-    sender: Sender<PolicyHit>,
-    receiver: Receiver<PolicyHit>,
-    logged: Option<Vec<PolicyHit>>,
+pub enum PolicyType {
+    MaskingRule,
+    FilterRule,
 }
 
-impl PolicyHitLogger {
-    pub fn new() -> Self {
+pub struct PolicyFound {
+    rule_definition: serde_json::Value,
+    policy_name: Option<String>,
+    policy_type: PolicyType,
+    policy_id: String,
+}
+
+pub enum PolicyCollectResult {
+    /// String = object_id, PolicyRule contains all hit rules
+    Found(Vec<(String, Vec<PolicyFound>)>),
+    CacheInvalid,
+    NotFound,
+}
+
+pub(crate) struct PolicyHitManager {
+    sender: Sender<PolicyHit>,
+    receiver: Receiver<PolicyHit>,
+    cached: Box<dyn Cache<u64, (String, HitRule)>>, // repo: RepoRest,
+}
+
+impl PolicyHitManager {
+    pub fn new(cached: Box<dyn Cache<u64, (String, HitRule)>>) -> Self {
         let (sender, receiver) = channel::<PolicyHit>();
-        PolicyHitLogger {
+        PolicyHitManager {
             sender,
             receiver,
-            logged: None,
+            cached,
         }
     }
 
@@ -42,20 +63,112 @@ impl PolicyHitLogger {
         self.sender.clone()
     }
 
-    pub fn process_hits(&mut self) {
-        if self.logged.is_some() {
-            return;
-        }
-
+    pub fn process_hits(&mut self) -> Result<PolicyCollectResult, String> {
         let mut logged = Vec::new();
         while let Ok(hit) = self.receiver.try_recv() {
             logged.push(hit);
         }
-        self.logged = Some(logged);
+
+        if logged.is_empty() {
+            return Ok(PolicyCollectResult::NotFound);
+        }
+
+        // process cache with new hits
+        let mut items = logged.iter().peekable();
+        let mut object_ids = Vec::new();
+        let mut hits = Vec::with_capacity(items.len());
+        while let Some(hit) = items.next() {
+            match (hit, items.peek()) {
+                (PolicyHit::HitEnforce(enforce), Some(PolicyHit::HitRule(ref rule))) => {
+                    self.register_rule(enforce, rule.clone());
+                    hits.push(rule.id);
+                    object_ids.push(enforce.object_id.clone());
+                }
+                (PolicyHit::HitEnforce(enforce), _) => {
+                    if let Some((object_id, rule)) = self.cached.get(&enforce.id) {
+                        hits.push(rule.id);
+                        object_ids.push(object_id);
+                    } else {
+                        // if this happens, clear cache and retry
+                        return Ok(PolicyCollectResult::CacheInvalid);
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        let mut toreturn = Vec::with_capacity(hits.len());
+        for id in hits {
+            let policy = self.collect_policy(&id)?;
+            toreturn.push(policy);
+        }
+
+        Ok(PolicyCollectResult::Found(toreturn))
     }
 
-    pub fn get_logged_hits(&mut self) -> Option<Vec<PolicyHit>> {
-        self.logged.take()
+    fn register_rule(&self, enforce: &HitEnforce, rule: HitRule) {
+        if enforce.id != rule.id {
+            panic!("rule id mismatch");
+        }
+        self.cached.set(rule.id, (enforce.object_id.clone(), rule));
+    }
+
+    fn collect_policy(&self, id: &u64) -> Result<(String, Vec<PolicyFound>), String> {
+        if let Some(hit) = self.cached.get(&id) {
+            let mut policies = Vec::new();
+            for rule in hit.1.rules {
+                let rule = rule.split(',').collect::<Vec<&str>>();
+                let len = rule.len();
+                if len < 3 {
+                    return Err("invalid policy format".to_string());
+                }
+
+                let rule_definition = self.parse_rule_definition(rule[len - 2])?;
+                let (policy_name, policy_type) =
+                    self.get_policy_type_and_name_from_rule_definition(&rule_definition)?;
+
+                policies.push(PolicyFound {
+                    policy_id: rule[len - 1].to_string(),
+                    rule_definition,
+                    policy_type,
+                    policy_name,
+                })
+            }
+            return Ok((hit.0.clone(), policies));
+        }
+        // this can technically never happen, since the cache is validated beforehand
+        Err("policy not found in cache".to_string())
+    }
+
+    fn parse_rule_definition(&self, rule_definition: &str) -> Result<serde_json::Value, String> {
+        let rule = BASE64_STANDARD
+            .decode(rule_definition.trim())
+            .map_err(|e| format!("Error while base64 decoding string: {}", e.to_string()))?;
+        let rule = String::from_utf8(rule).map_err(|e| {
+            format!(
+                "Error while parsing string from decoded base64 message {}",
+                e.to_string()
+            )
+        })?;
+        Ok(serde_json::from_str(&rule).map_err(|e| {
+            format!(
+                "Error while parsing rule definition to json: {}",
+                e.to_string()
+            )
+        })?)
+    }
+
+    fn get_policy_type_and_name_from_rule_definition(
+        &self,
+        rule_definition: &serde_json::Value,
+    ) -> Result<(Option<String>, PolicyType), String> {
+        if let Some(name) = rule_definition.get("name") {
+            if let serde_json::Value::String(name) = name {
+                return Ok((Some(name.clone()), PolicyType::MaskingRule));
+            }
+            return Err("policy name is not a string".to_string());
+        }
+        Ok((None, PolicyType::FilterRule))
     }
 }
 
@@ -86,10 +199,9 @@ impl Logger for PolicyLogger {
         let mut hasher = DefaultHasher::new();
         rvals.hash(&mut hasher);
 
-        // todo: use regex instead?
         let object = rvals
             .last()
-            .unwrap()
+            .unwrap_or(&",,unknown".to_string())
             .split(',')
             .skip(1)
             .take(1)
@@ -132,105 +244,63 @@ impl Logger for PolicyLogger {
     }
 }
 
-// TODO: the policy manager should be responsible for getting the transpiler output and generating output on the proxy endpoint
-// features it should support:
-// - resolve conflicts (set precedence based on settings)
-// - generate request url if some assets are not found or denied (done via the API)
-// - update last accessed time for assets
-// - allow for impersonation (merge own rules with that of another user, least privileged takes precedence)
-// - cache enforcer rules (using ids), cache evict enforcer cache if an enforced rule is missing
+#[cfg(test)]
+mod tests {
+    use crate::security::policies::{PolicyCollectResult, PolicyHitManager, PolicyLogger};
+    use casbin::Logger;
 
-pub struct PolicyManager {
-    cached: Box<dyn Cache<u64, (String, HitRule)>>, // repo: RepoRest,
-}
+    #[test]
+    fn test_policy_logger_capture_hit_and_enforce() {
+        let (sender, receiver) = std::sync::mpsc::channel();
+        let sut = PolicyLogger::new(sender);
+        send_test_data(sut);
 
-pub struct PolicyFound {
-    expression: String,
-    policy_id: String,
-}
-
-pub enum PolicyCollectResult {
-    /// String = object_id, PolicyRule contains all hit rules
-    Found(Vec<(String, Vec<PolicyFound>)>),
-    CacheInvalid,
-    NotFound,
-}
-
-impl PolicyManager {
-    pub fn new(cached: Box<dyn Cache<u64, (String, HitRule)>>) -> Self {
-        Self { cached }
-    }
-
-    pub async fn process(
-        &self,
-        mut policy_logger: PolicyHitLogger,
-    ) -> Result<PolicyCollectResult, String> {
-        policy_logger.process_hits();
-        let logged_hits = if let Some(logged_hits) = policy_logger.get_logged_hits() {
-            logged_hits
-        } else {
-            return Ok(PolicyCollectResult::NotFound);
-        };
-
-        // process cache with new hits
-        let mut items = logged_hits.iter().peekable();
-        let mut object_ids = Vec::new();
-        let mut hits = Vec::with_capacity(items.len());
-        while let Some(hit) = items.next() {
-            match (hit, items.peek()) {
-                (PolicyHit::HitEnforce(enforce), Some(PolicyHit::HitRule(ref rule))) => {
-                    self.register_rule(enforce, rule.clone());
-                    hits.push(rule.id);
-                    object_ids.push(enforce.object_id.clone());
-                }
-                (PolicyHit::HitEnforce(enforce), _) => {
-                    if let Some((object_id, rule)) = self.cached.get(&enforce.id) {
-                        hits.push(rule.id);
-                        object_ids.push(object_id);
-                    } else {
-                        return Ok(PolicyCollectResult::CacheInvalid);
-                    }
-                }
-                _ => {}
-            }
+        // check captured logs
+        let mut hits = Vec::new();
+        while let Ok(hit) = receiver.recv() {
+            hits.push(hit);
         }
 
-        self.process_hits(object_ids).await;
-        let mut toreturn = Vec::with_capacity(hits.len());
-        for id in hits {
-            let policy = self.collect_policy(&id)?;
-            toreturn.push(policy);
-        }
-
-        Ok(PolicyCollectResult::Found(toreturn))
+        assert_eq!(hits.len(), 4);
     }
 
-    fn register_rule(&self, enforce: &HitEnforce, rule: HitRule) {
-        if enforce.id != rule.id {
-            panic!("rule id mismatch");
+    #[test]
+    fn test_policy_hit_manager_results() {
+        let cache = Box::new(casbin::DefaultCache::new(400));
+        let mut sut = PolicyHitManager::new(cache);
+        {
+            let logger = PolicyLogger::new(sut.get_sender());
+            send_test_data(logger);
         }
-        self.cached.set(rule.id, (enforce.object_id.clone(), rule));
+
+        let results = sut.process_hits().unwrap();
+        if let PolicyCollectResult::Found(policies) = results {
+            assert_eq!(policies.len(), 2);
+        }
     }
 
-    async fn process_hits(&self, object_ids: Vec<String>) {
-        // TODO: send data-accessed post request to API for updating last accessed time, lets do this via the proxy channel and async processing
-    }
+    fn send_test_data(mut logger: PolicyLogger) {
+        logger.enable_log(true);
 
-    fn collect_policy(&self, id: &u64) -> Result<(String, Vec<PolicyFound>), String> {
-        if let Some(hit) = self.cached.get(&id) {
-            let mut policies = Vec::new();
-            for rule in hit.1.rules {
-                let rule = rule.split(',').collect::<Vec<&str>>();
-                if rule.len() < 3 {
-                    return Err("invalid policy format".to_string());
-                }
-                policies.push(PolicyFound {
-                    expression: rule[rule.len() - 2].to_string(),
-                    policy_id: rule[rule.len() - 1].to_string(),
-                })
-            }
-            return Ok((hit.0.clone(), policies));
-        }
-        Err("policy not found in cache".to_string())
+        // send log events
+        // Masking example
+        let rvals = vec![
+            r##"#{"accountType": "User", "id": "some_id", "principalName": "alice", "role": "", "tags": ["pii::username", "pii::email"]}"##.to_string(),
+            r##"#{"entityVersion": 0, "groups": [#{"id": "some_id", "tags": ["pii::username", "pii::email"]}], "userId": "some_id"}"##.to_string(),
+            r##"#{"appDriver": "", "appId": 0, "appName": "", "appType": "", "branch": "", "computeId": "", "continent": "", "countryIso2": "", "dayOfWeek": 0, "domainId": "", "id": "some_id", "policyId": "", "sourceIpv4": "", "time": 0, "timezone": "", "workspaceId": ""}"##.to_string(),
+            r##"#{"full_name": "some.schema.column", "id": "some_object_id", "is_aggregated": false, "last_time_accessed": 0, "tags": ["pii::username", "pii::email"]}"##.to_string()
+        ];
+        logger.print_enforce_log(&rvals, true, false);
+        logger.print_explain_log(&rvals, vec!["p, some.*, TagExists(r.user, \"Hello\") && 1 == 1 && TagExists(r.group, \"Something\"), allow, eyJuYW1lIjogInh4aGFzaDMiLCAicHJvcGVydGllcyI6IG51bGx9, policy_id_1".to_string()]);
+
+        // Filter example
+        let rvals = vec![
+            r##"#{"accountType": "User", "id": "some_id", "principalName": "alice", "role": "", "tags": ["pii::username", "pii::email"]}"##.to_string(),
+            r##"#{"entityVersion": 0, "groups": [#{"id": "some_id", "tags": ["pii::username", "pii::email"]}], "userId": "some_id"}"##.to_string(),
+            r##"#{"appDriver": "", "appId": 0, "appName": "", "appType": "", "branch": "", "computeId": "", "continent": "", "countryIso2": "", "dayOfWeek": 0, "domainId": "", "id": "some_id", "policyId": "", "sourceIpv4": "", "time": 0, "timezone": "", "workspaceId": ""}"##.to_string(),
+            r##"#{"full_name": "some.schema.column", "id": "another_object_id", "is_aggregated": false, "last_time_accessed": 0, "tags": ["pii::username", "pii::email"]}"##.to_string()
+        ];
+        logger.print_enforce_log(&rvals, true, false);
+        logger.print_explain_log(&rvals, vec!["p, some.*, TagExists(r.user, \"Hello\") && 1 == 1 && TagExists(r.group, \"Something\"), allow, eyJleHByZXNzaW9uIjogIj8gPCAxMDAwIn0=, policy_id_2".to_string()]);
     }
 }
