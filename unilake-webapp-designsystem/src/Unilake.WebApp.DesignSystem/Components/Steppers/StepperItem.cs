6af5b333792e394a44cafeaf27274fcca45b6545
Menno Hamburg <menno@mhamburg.nl>
2025-01-08 16:33:08 +0100

    feat(web): added initial stepper component

---
+++ unilake-webapp-designsystem/src/Unilake.WebApp.DesignSystem/Components/Steppers/StepperItem.cs
@@ -0,0 +1,7 @@
+namespace Unilake.WebApp.DesignSystem.Components.Steppers;
+
+public class StepperItem
+{
+    public required string Text { get; set; }
+    public string? HelpText { get; set; }
+}
