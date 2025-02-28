a491992d3196adc794ce12b8f075a3415ee28c82
Menno Hamburg <menno@mhamburg.nl>
2025-02-28 16:40:42 +0100

    web: added timeline component

---
+++ unilake-webapp-designsystem/src/Unilake.WebApp.DesignSystem/Components/Timelines/TimeLineItem.cs
@@ -0,0 +1,8 @@
+namespace Unilake.WebApp.DesignSystem.Components;
+
+public class TimeLineItem
+{
+    public required string VersionNumber { get; init; }
+    public required string Comment { get; init; }
+    public required string Uri { get; init; }
+}
