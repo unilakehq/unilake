35a08cca6eeb90a6c5dd25117b366a3a6a4429a0
Menno Hamburg <menno@mhamburg.nl>
2025-03-30 23:52:19 +0200

    web: added state descriptions

--- unilake-webapp/src/Unilake.WebApp/Services/State.cs
+++ unilake-webapp/src/Unilake.WebApp/Services/State.cs
@@ -2,8 +2,24 @@ namespace Unilake.WebApp.Services;
 
 public static class State
 {
+    /// <summary>
+    /// Determines if the application is in dark mode or not.
+    /// Datatype: bool
+    /// </summary>
     public const string DarkMode = "DarkMode";
+    /// <summary>
+    /// Determines if the side navigation is collapsed or not.
+    /// Datatype: bool
+    /// </summary>
     public const string SideNavCollapsed = "SideNavCollapsed";
+    /// <summary>
+    /// Navigation menu items that are currently available.
+    /// Datatype: IEnumerable[MenuItem]
+    /// </summary>
     public const string NavigationMenu = "NavigationMenu";
+    /// <summary>
+    /// Current culture of the application.
+    /// Datatype: string
+    /// </summary>
     public const string Culture = "fr";
 }
