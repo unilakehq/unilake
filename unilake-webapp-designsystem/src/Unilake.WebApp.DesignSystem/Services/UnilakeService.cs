1a8f7a0eb4bea95ed8e353f29767d46e7f134638
Menno Hamburg <menno@mhamburg.nl>
2025-04-01 15:29:36 +0200

    chore: code improvements

--- unilake-webapp-designsystem/src/Unilake.WebApp.DesignSystem/Services/UnilakeService.cs
+++ unilake-webapp-designsystem/src/Unilake.WebApp.DesignSystem/Services/UnilakeService.cs
@@ -3,32 +3,25 @@ using Microsoft.JSInterop;
 
 namespace Unilake.WebApp.DesignSystem.Services;
 
-public class UnilakeService
+public class UnilakeService(IJSRuntime jsRuntime)
 {
-    private readonly IJSRuntime _jsRuntime;
-
-    public UnilakeService(IJSRuntime jsRuntime)
-    {
-        _jsRuntime = jsRuntime;
-    }
-
     public async Task CopyToClipboard(string text)
     {
-        await _jsRuntime.InvokeVoidAsync("unilake.copyToClipboard", text);
+        await jsRuntime.InvokeVoidAsync("unilake.copyToClipboard", text);
     }
 
     public async Task<string> ReadFromClipboard()
     {
-        return await _jsRuntime.InvokeAsync<string>("unilake.readFromClipboard");
+        return await jsRuntime.InvokeAsync<string>("unilake.readFromClipboard");
     }
 
     public async Task SetElementProperty(ElementReference element, string property, object value)
     {
-        await _jsRuntime.InvokeVoidAsync("unilake.setPropByElement", element, property, value);
+        await jsRuntime.InvokeVoidAsync("unilake.setPropByElement", element, property, value);
     }
 
     public async Task DisableDraggable(ElementReference container, ElementReference element)
     {
-        await _jsRuntime.InvokeVoidAsync("unilake.disableDraggable", container, element);
+        await jsRuntime.InvokeVoidAsync("unilake.disableDraggable", container, element);
     }
 }
